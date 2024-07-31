b96dd7d2f394760160390734f2610672d4e3153b
MRH-1722 <muhammadrehan1722@gmail.com>
2024-07-31 13:36:31 +0500

    admin file modifications

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Detail
+from .models import Detail , Body_detail , Variant
 
 # Register your models here.
 admin.site.register(Detail)
+admin.site.register(Body_detail)
+admin.site.register(Variant)
