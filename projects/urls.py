3ce4f37fc35fe643c608c43bca0ae8699ec45269
MRH-1722 <muhammadrehan1722@gmail.com>
2024-08-06 17:45:28 +0500

    forth commit

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('' , views.details ,name="details" ),
     path('detail/<str:pk>/' , views.detail ,name="detail" ),
+
+    path('create-detail/' , views.createDetail , name="create-detail"),
+    path('update-detail/<str:pk>/' , views.updateDetail , name="update-detail"),
+    path('delete-detail/<str:pk>/' , views.deleteDetail , name="delete-detail"),
+    
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -1,30 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Detail
-
-carDetailList = [
-    { 
-        'id' : '1',
-        'Company' : 'Toyota',
-        'Model' : 'Corola',
-        'Year' : '2012',
-        'Description' : 'This car has outstanding suspension'
-    },
-    { 
-        'id' : '2',
-        'Company' : 'Honda',
-        'Model' : 'Civic',
-        'Year' : '2013',
-        'Description' : 'This car is the king for the racing'
-    }, 
-    { 
-        'id' : '3',
-        'Company' : 'Toyota',
-        'Model' : 'Prius',
-        'Year' : '2014',
-        'Description' : 'This car has outstanding fuel efficiency'
-    },
-]
+from .forms import DetailForm
 
 def details(request):
     allDetails = Detail.objects.all()
@@ -34,3 +11,33 @@ def detail(request, pk):
     detailObj = Detail.objects.get(uuid=pk)
     variant = Detail.objects.all()
     return render(request, 'detail.html' , {'detail' : detailObj , 'variant':variant})
+
+def createDetail(request):
+    form = DetailForm()
+    if request.method == 'POST':
+        form = DetailForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('details')
+    context = {'form' : form}
+    return render(request, 'detail_form.html' , context)
+
+def updateDetail(request, pk):
+    detail = Detail.objects.get(uuid=pk)
+    form = DetailForm(instance=detail)
+    if request.method == 'POST':
+        form = DetailForm(request.POST, instance=detail)
+        if form.is_valid():
+            form.save()
+            return redirect('details')
+    context = {'form':form}
+    return render(request, 'detail_form.html' , context)
+
+def deleteDetail(request , pk):
+    detail = Detail.objects.get(uuid=pk)
+    if request.method == 'POST':
+        detail.delete()
+        return redirect('details')
+    context = {'detail' : detail}
+    return render(request , 'delete_detail.html' , context)
+ 
